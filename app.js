0cbfa8b3e101d811e1f9cd0f3369c5126ab03434
Hasan <hasan.pakistani@gmail.com>
2020-01-14 23:25:51 +0500

    Initial build

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const NodeMediaServer = require('./');
 
 const config = {
   rtmp: {
-    port: process.env.PORT || 1935,
+    port: 1935,
     chunk_size: 60000,
     gop_cache: true,
     ping: 30,
     ping_timeout: 60
   },
   http: {
-    port: 4000,
+    port: process.env.PORT || 4000,
     mediaroot: './media',
     webroot: './www',
     allow_origin: '*'
